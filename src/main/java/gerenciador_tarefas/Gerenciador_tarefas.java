94548fbaaa8eafb2c450f2879ab8973b200fb198
daniel99korban <d.lima@acad.ifma.edu.br>
2022-10-22 17:15:59 -0300

    first commit

---
+++ src/main/java/gerenciador_tarefas/Gerenciador_tarefas.java
@@ -0,0 +1,13 @@
+
+package gerenciador_tarefas;
+
+/**
+ *
+ * @author Daniel Lima, Jose Victor & Paulo Roberto
+ */
+public class Gerenciador_tarefas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/view/LoginView.java
@@ -0,0 +1,27 @@
+
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Daniel Lima, Jose Victor & Paulo Roberto
+ */
+public class LoginView extends JFrame{
+    
+    // Apenas um exemplo
+    public LoginView(String titulo){
+        this.setTitle(titulo);
+        this.setSize(500, 500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new LoginView("Tela de Login");
+    }
+    
+}
+
+
