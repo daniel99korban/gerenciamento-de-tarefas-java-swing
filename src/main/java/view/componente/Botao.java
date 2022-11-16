0636887c2500619019ea6ffc6fe87a20424073d8
daniel99korban <d.lima@acad.ifma.edu.br>
2022-11-16 16:35:09 -0300

    componentes foram separados no pacote view.componente

--- src/main/java/view/componente/Botao.java
+++ src/main/java/view/componente/Botao.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package view;
+package view.componente;
 
 import java.awt.Color;
 import javax.swing.JButton;
--- src/main/java/view/componente/Label.java
+++ src/main/java/view/componente/Label.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package view;
+package view.componente;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -15,12 +15,16 @@ import javax.swing.JLabel;
 public class Label extends JLabel {
     /**
      * @param titulo será exatamente o texto que aparecerá na tela.
-     * @param cor o programador tem a opção de alterar cor, caso use o outro construtor, por default a cor será black.
+     * @param cor o programador tem as opções adicionais de alterar cor e font, caso use o outro construtor, por default a cor será black.
      */
     public Label(String titulo, Color cor, Font fonte){
+        this(titulo, cor);
+        this.setFont(fonte);
+    }
+    
+    public Label(String titulo, Color cor){
         this.setText(titulo);
         this.setForeground(cor);
-        this.setFont(fonte);
     }
     
     public Label(String titulo){
