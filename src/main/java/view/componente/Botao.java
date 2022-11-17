ae71ebe2e47febdf0a25c84aadb5db72cfa42031
daniel99korban <d.lima@acad.ifma.edu.br>
2022-11-17 15:08:36 -0300

    novos construtores adicinados

--- src/main/java/view/componente/Botao.java
+++ src/main/java/view/componente/Botao.java
@@ -8,12 +8,28 @@ import java.awt.Color;
 import javax.swing.JButton;
 
 public class Botao extends JButton {
+    
     private Color azulEscuro = new Color(36, 9, 147);
+    
     public Botao(String titulo){
         this.setText(titulo);
         this.setSize(80, 20);
         this.setForeground(Color.WHITE);
         this.setBackground(azulEscuro);
+        this.setVisible(true);  
+    }
+    
+    public Botao(String titulo, Color corFrente){
+        this.setText(titulo);
+        this.setForeground(corFrente);
+        this.setBackground(azulEscuro);
+        this.setVisible(true);  
+    }
+    
+    public Botao(String titulo, Color corFrente, Color corFundo){
+        this.setText(titulo);
+        this.setForeground(corFrente);
+        this.setBackground(corFundo);
         this.setVisible(true);  
     }
 }
